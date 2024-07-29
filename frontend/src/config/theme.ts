c30f027ca791edfbf669dfe94dc85748f43ecf69
Salistito <sebastian.salinas.r@ug.uchile.cl>
2024-07-29 01:59:27 -0400

    refactor: initalize app and mui theme

---
+++ frontend/src/config/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  palette: {
+    text: {
+      primary: "#7355FB",
+      secondary: "#F1EEFF",
+      black: "#414046",
+    },
+    background: {
+      primary: "#7355FB",
+      secondary: "#F1EEFF",
+      white: "#FFFFFF",
+    },
+    success: {
+      primary: "#4CAF50",
+    },
+    errors: {
+      primary: "#D32F2F",
+    },
+  },
+};
